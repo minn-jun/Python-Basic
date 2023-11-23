ba45b6f4a26f4bb7445c74a420e83ebb05cc3a83
Minjun Ko <kohmj0324@naver.com>
2023-11-23 15:34:06 +0900

    I

---
+++ I/data_type.py
@@ -0,0 +1,25 @@
+# 숫자
+print(5)
+print(-10)
+print(3.14)
+print(10000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+
+# 문자열
+print("풍선")
+print('나비')
+print("aaaaaaaaa")
+print('a'*9)
+
+
+# Boolean
+print(5 > 10)   # False
+print(5 < 10)   # True
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
---
+++ I/variable.py
@@ -0,0 +1,11 @@
+# 변수
+name = "Amy"
+age = 4
+hobby = "Game"
+
+print(f"{name} is {age} years old.")
+print(f"He(She) loves {hobby}")
+
+print(f"{name} is {age} years old.")
+hobby = "Soccer"
+print(f"He(She) loves {hobby}")
